3345464614bd8bfdff149809132c6553ddafb1ab
AitorVadillo <82975584+AitorVadillo@users.noreply.github.com>
2021-04-22 22:35:19 +0200

    New Version

---
+++ RDGrid.py
@@ -0,0 +1,23 @@
+"""Provides a scripting component.
+    Inputs:
+        x: The x script variable
+        y: The y script variable
+    Output:
+        a: The a output variable"""
+
+#import Rhino.Geometry as rg
+import random as rnd
+
+#Crear matriz actual y pasada
+#Crear swap de matrices con otra variable más
+#Crear nueva grid a partir de la pasada
+#Crear convolucion de laplace
+#Crear las variables pa la RF
+
+#Roca Billy
+
+Width = 200
+Heigth = 200
+
+grid = []
+
